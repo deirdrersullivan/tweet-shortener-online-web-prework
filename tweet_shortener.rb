93a9b44ac47ea390fc04b13725da6419c81100db
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-06-07 03:12:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,3 +12,12 @@ dictionary = {
 }
 end
 
+def word_substituter(tweet)
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
